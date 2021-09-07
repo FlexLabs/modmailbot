dd4f6efdf2e16a319b6f19dcf9d703bf6cdc15e0
iiFDCT <conor.turner11@gmail.com>
2021-09-07 15:38:31 +0100

    Require an argument at all times

--- src/modules/hide.js
+++ src/modules/hide.js
@@ -9,12 +9,11 @@ const threadUtils = require("../threadUtils");
 module.exports = bot => {
   threadUtils.addInboxServerCommand(bot, "hide", async (msg, args, thread) => {
     if (! utils.isAdmin(msg.member)) return;
-    if (! thread) {
-      if (! args[0]) return bot.createMessage(msg.channel.id, "Please provide a thread ID!");
+    if (! args[0]) return bot.createMessage(msg.channel.id, "Please provide a thread ID!");
 
-      thread = await threads.findById(args[0]);
-      if (! thread) return bot.createMessage(msg.channel.id, "Thread not found!");
-    }
+    thread = await threads.findById(args[0]);
+
+    if (! thread) return bot.createMessage(msg.channel.id, "Thread not found!");
 
     const suffix = msg.channel.id == thread.channel_id ? "this" : "that";
     const text = `Made ${suffix} thread`;
