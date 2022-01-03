f79abe898f57f6951ff99a7e8fda780daa9c1909
iiFDCT <conor.turner11@gmail.com>
2022-01-03 16:06:52 +0000

    Use Discord timestamp markdown

--- src/modules/close.js
+++ src/modules/close.js
@@ -67,9 +67,9 @@ module.exports = (bot, sse) => {
       } else if (args[0] === "status") {
         let message;
         if (thread.scheduled_close_at) {
-          message = `Thread scheduled to close\nClosing at ${thread.scheduled_close_at}\nClosing by ${thread.scheduled_close_name} (${thread.scheduled_close_id})`;
+          message = `Closing at <t:${moment(thread.scheduled_close_at).unix()}:F>\nClosing by ${thread.scheduled_close_name} (${thread.scheduled_close_id})`;
         } else {
-          message = "Thread is not scheduled to close!";
+          message = "This thread is not scheduled to close!";
         }
         thread.postSystemMessage(message);
         return;
