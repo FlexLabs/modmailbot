755db9141319c2d615ccb8460b8dc9799344a3aa
Bsian <chharry321@gmail.com>
2022-02-24 23:11:09 +0000

    Add uuid to loglink functionality

--- src/modules/logs.js
+++ src/modules/logs.js
@@ -76,8 +76,15 @@ module.exports = bot => {
     return getLogs(userId);
   });
 
-  threadUtils.addInboxServerCommand(bot, "loglink", async (msg, args, thread) => {
+  bot.registerCommand("loglink", async (msg, args) => {
+    if (! (await utils.messageIsOnInboxServer(msg))) return;
+    if (! utils.isStaff(msg.member)) return;
+
+    if (args[0]) return msg.channel.createMessage(utils.getSelfUrl(`#thread/${args[0]}`));
+
+    const thread = await threads.findOpenThreadByChannelId(msg.channel.id);
     if (! thread) return;
+
     const logUrl = await thread.getLogUrl();
     thread.postSystemMessage(`Log URL: <${logUrl}>`);
   });
