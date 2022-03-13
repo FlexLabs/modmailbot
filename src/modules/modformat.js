bbf3f30b33424de9b93b9c9ecc6be52f3e095851
iiFDCT <conor.turner11@gmail.com>
2022-03-13 17:25:12 +0000

    Fix spacing

--- src/modules/modformat.js
+++ src/modules/modformat.js
@@ -9,20 +9,26 @@ module.exports = bot => {
     if (! thread) return;
     if (args[0] === "id") {
       args.shift();
+
       const reason = args.join(" ").trim();
       const messages = await thread.getThreadMessages();
-      let idArray = [];
+      const idArray = [];
+
       for await (const message of messages) {
         if (/(?:w|whois|modlogs|modinfo)\s<?@?!?\d{16,}>?/.test(message.body)) {
           let lastItem = message.body.split(" ")[1].match(/\d{16,}/);
           if(lastItem != thread.user_id) {
-            idArray.push(lastItem);} 
-        }}
+            idArray.push(lastItem);
+          }
+        }
+      }
+
       bot.createMessage(msg.channel.id, `${idArray.pop() || ""} ${reason} | ${thread.id}`.trim());
     } else {
       const reason = args.join(" ").trim();
       bot.createMessage(msg.channel.id, `${reason} | ${thread.id}`);
     }
   });
+
   bot.registerCommandAlias("mf", "modformat");
 };
