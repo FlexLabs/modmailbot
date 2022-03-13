c81288e048ee14e882fdf3b2c2eeefe80ce6f8bf
iiFDCT <conor.turner11@gmail.com>
2022-03-13 17:34:52 +0000

    Return content AND embed (if there is one)

--- src/data/Thread.js
+++ src/data/Thread.js
@@ -278,7 +278,7 @@ class Thread {
       message_type: THREAD_MESSAGE_TYPE.SYSTEM,
       user_id: null,
       user_name: "",
-      body: typeof text === "string" ? text : (text.content + text.embed ? " <embed>" : "").trim(),
+      body: typeof text === "string" ? text : (text.content + (text.embed ? "\n\n<embed>" : "")).trim(),
       is_anonymous: 0,
       dm_message_id: msg.id,
       thread_message_id: msg.id
